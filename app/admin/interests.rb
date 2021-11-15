bb967c21aead0f01d6721346c05a79560f4de970
anonymous anonymous <alexfeldman@me.com>
2021-11-15 22:08:15 +0000

    Whitelist columns in Interest for ActiveAdmin

--- app/admin/interests.rb
+++ app/admin/interests.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Interest do
 
+ permit_params :user_id, :event_id, :response
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
