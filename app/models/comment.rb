a7dc38e39ec29b631b9336f6daca9bc3312b28d8
anonymous anonymous <alexfeldman@me.com>
2021-11-15 22:07:55 +0000

    Direct association between events and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,11 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :comment_board,
+             :class_name => "Event",
+             :foreign_key => "event_id",
+             :counter_cache => true
+
   belongs_to :commenter,
              :class_name => "User",
              :foreign_key => "user_id"
